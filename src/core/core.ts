e5dd07521255a51ab6773f9c30bec3ab13e45d56
SoviivVG <62228138+soviivvg@users.noreply.github.com>
2021-11-28 13:34:39 -0600

    mc1.18-v0.1.0+SNAPSHOT
    + No collision team
    + Send command feedback

---
+++ src/core/core.ts
@@ -0,0 +1,51 @@
+import { gamerule, MCFunction, team } from "sandstone"
+import { userNamespace } from "../project/global"
+
+/* ============================================================================ */
+/*                                   Variables                                  */
+/* ============================================================================ */
+
+export const corePackNamespace = 'core'
+export const toolsPackNamespace = 'tools'
+export const coreNamespace = `${userNamespace}:${corePackNamespace}/`
+export const utilsNamespace = `${coreNamespace}${toolsPackNamespace}/`
+
+
+/* ----------------------------- No Collision Team ---------------------------- */
+const noCollisionTeam = userNamespace + 'NoCollision'
+function noCollisionTeamRules(): void {
+  team.add(noCollisionTeam)
+  team.modify(noCollisionTeam, 'collisionRule', 'never')
+  team.modify(noCollisionTeam, 'seeFriendlyInvisibles', false)
+  team.modify(noCollisionTeam, 'friendlyFire', false)
+}
+
+/* --------------------------- Send Command Feedback -------------------------- */
+export const commandFeedbackTrue = MCFunction(coreNamespace + 'commandfeedback/true', () => {
+  gamerule('sendCommandFeedback', true)
+  gamerule('commandBlockOutput', true)
+})
+
+export const commandFeedbackFalse = MCFunction(coreNamespace + 'commandfeedback/false', () => {
+  gamerule('sendCommandFeedback', false)
+  gamerule('commandBlockOutput', false)
+})
+
+/* ============================================================================ */
+/*                                     Main                                     */
+/* ============================================================================ */
+
+MCFunction(coreNamespace + 'main', () => {
+  // Team Creation
+  noCollisionTeamRules()
+
+}, { runOnLoad: true })
+
+
+MCFunction(utilsNamespace + 'main', () => { }, { runOnLoad: true })
+
+/* ============================================================================ */
+
+/* ============================================================================ */
+/*                                    Unload                                    */
+/* ============================================================================ */
